ee60c12b125c0d4b575e098878a20cdb10fcbc66
GithsaraS <Ishancharun@icloud.com>
2023-11-29 05:14:06 +0530

    Add icons

--- components/Pareint.js
+++ components/Pareint.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHome } from '@fortawesome/free-solid-svg-icons' // Corrected typo in icon name
+import { faBars, faHome, faShop } from '@fortawesome/free-solid-svg-icons' // Corrected typo in icon name
 
 function Parent({ children }) { // Destructure children from props
     return (
         <div>
             <div>
-                <aside>
-                    <div className="flex flex-row place-items-center">
+                <aside className="p-6">
+                    <div className="flex flex-row space-x-3 place-items-center">
                         <FontAwesomeIcon
                             style={{
                                 height: 40,
@@ -19,58 +19,34 @@ function Parent({ children }) { // Destructure children from props
                         />
                         <a>Home</a>
                     </div>
-                    <div className="flex flex-row place-items-center">
+                    <div className="flex flex-row space-x-3 place-items-center">
                         <FontAwesomeIcon
                             style={{
                                 height: 40,
                                 width: 40
                             }}
-                            icon={faHome} // Corrected icon name
+                            icon={faBars} // Corrected icon name
                             size='xs'
                             color="white"
                         />
                         <a>POS</a>
                     </div>
-                    <div className="flex flex-row place-items-center">
+                    <div className="flex flex-row space-x-3 place-items-center">
                         <FontAwesomeIcon
                             style={{
                                 height: 40,
                                 width: 40
                             }}
-                            icon={faHome} // Corrected icon name
+                            icon={faShop} // Corrected icon name
                             size='xs'
                             color="white"
                         />
                         <a>Stock</a>
                     </div>
-                    <div className="flex flex-row place-items-center">
-                        <FontAwesomeIcon
-                            style={{
-                                height: 40,
-                                width: 40
-                            }}
-                            icon={faHome} // Corrected icon name
-                            size='xs'
-                            color="white"
-                        />
-                        <a>Home</a>
-                    </div>
-                    <div className="flex flex-row place-items-center">
-                        <FontAwesomeIcon
-                            style={{
-                                height: 40,
-                                width: 40
-                            }}
-                            icon={faHome} // Corrected icon name
-                            size='xs'
-                            color="white"
-                        />
-                        <a>Home</a>
-                    </div>
-
+                    
                 </aside>
             </div>
-            <div>
+            <div className="ml-72 p-12 pt-16">
                 {children} {/* Render children passed to Parent */}
             </div>
         </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,7 +12,12 @@ export default function Home() {
         </title>
       </Head>
       <Pareint>
-        
+        <div className='flex text-white font-bold'>
+        <div className='p-10 bg-slate-900 rounded-x1'>
+          <h1 className='text-3xl'>Total Sold</h1>
+          <h1 className='text-2xl'>$123456</h1>
+        </div>
+        </div>
       </Pareint>
     </div>
   )
